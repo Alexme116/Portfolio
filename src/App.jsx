69e2f27a59d54c015d279e8f38dd972250f7811f
Alexme116 <alexmoncada16@hotmail.com>
2025-04-29 07:38:51 -0600

    Font Fixed And Html And Body Background Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import Home from './components/Home/Home'
@@ -7,7 +6,7 @@ import Projects from './components/Projects/Projects'
 
 function App() {
   return (
-    <div className='relative w-svw h-svh overflow-auto bg-[#0a0413]'>
+    <div className='relative w-svw h-svh overflow-auto'>
       {/* NavBar */}
       <NavBar/>
 
